5c79ec1b7b1dbd093d23f33e5f63d2e83d7f38cb
hectorpatricio <hectorlillo.vega71@gmail.com>
2026-02-26 15:42:17 -0300

    version 0.2

--- src/index.py
+++ src/index.py
@@ -1,15 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+tareas = []  # Lista temporal en memoria
+
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', tareas=tareas)
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+
+@app.route('/agregar', methods=['POST'])
+def agregar():
+    nueva_tarea = request.form.get('tarea')
+
+    if nueva_tarea:
+        tareas.append(nueva_tarea)
+
+    return redirect(url_for('home'))
+
+
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True)
+
+
+
+#FORMATO ANTIGUO 26/02/2026:
+
+# from flask import Flask, render_template
+# app = Flask(__name__)
+# @app.route('/')
+# def home():
+#     return render_template('home.html')
+# @app.route('/about')
+# def about():
+#     return render_template('about.html')
+# if __name__ == '__main__':
+#     app.run(debug=True) 
 
