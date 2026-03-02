1a75f56a3737cc4206407836d512452e07ab25b6
hectorpatricio <hectorlillo.vega71@gmail.com>
2026-03-02 14:46:30 -0300

    version 0.4

--- src/index.py
+++ src/index.py
@@ -1,19 +1,23 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.secret_key = "clave_super_secreta"
 
 tareas = []  # Lista temporal en memoria
 
+#------------------------------------------------------------------------------------ 
 
 @app.route('/')
 def home():
     return render_template('home.html', tareas=tareas)
 
+#------------------------------------------------------------------------------------ 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+#------------------------------------------------------------------------------------ 
 
 @app.route('/agregar', methods=['POST'])
 def agregar():
@@ -21,8 +25,25 @@ def agregar():
 
     if nueva_tarea != "":
         tareas.append(nueva_tarea)
+        #flash("Tarea agregada correctamente ✅", "success")
+    # else:
+    #     flash("Debes escribir una tarea antes de agregarla ⚠️", "danger")
 
     return redirect(url_for('home'))
+
+#------------------------------------------------------------------------------------ 
+
+@app.route('/modificar/<int:indice>', methods=['POST'])
+def modificar(indice):
+
+    tarea_editada = request.form.get('tarea_editada', '').strip()
+
+    if tarea_editada != "":
+        tareas[indice] = tarea_editada
+
+    return redirect(url_for('home'))
+
+#------------------------------------------------------------------------------------ 
 
 @app.route('/eliminar/<int:indice>', methods=['POST'])
 def eliminar(indice):
@@ -32,7 +53,7 @@ def eliminar(indice):
 if __name__ == '__main__':
     app.run(debug=True)
 
-
+#------------------------------------------------------------------------------------ 
 
 #FORMATO ANTIGUO 26/02/2026:
 
